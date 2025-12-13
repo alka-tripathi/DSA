f8e7384c0a5baac27d5bc6c862b6b1c0b2fe197c
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2025-12-13 10:14:52 +0530

    Time: 1 ms (89.54%) | Memory: 45.4 MB (50.60%) - LeetSync

---
+++ 46-permutations/permutations.java
@@ -0,0 +1,26 @@
+class Solution {
+    public static void solution(int arr[],boolean[] used,List<List<Integer>> base,ArrayList<Integer> list){
+    if(list.size()==arr.length){
+        base.add(new ArrayList<>(list));
+        return;
+    }
+
+    for(int i=0;i<arr.length;i++){
+        if(used[i]) continue;
+        used[i]=true;
+        list.add(arr[i]);
+        solution(arr,used,base,list);
+        used[i]=false;
+        list.remove(list.size()-1);
+    
+    }
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result=new ArrayList<>();
+          ArrayList<Integer> list = new ArrayList<>();
+          boolean used[]=new boolean[nums.length];
+         solution(nums,used,result,list);
+         return result;
+        
+    }
+}
