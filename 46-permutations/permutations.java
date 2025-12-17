713e20c4da3fbb7961931158602a1a08e9bfc08d
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2025-12-17 11:09:04 +0530

    Time: 1 ms (89.11%) | Memory: 45.6 MB (17.99%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,26 +1,26 @@
 class Solution {
-    public static void solution(int arr[],boolean[] used,List<List<Integer>> base,ArrayList<Integer> list){
-    if(list.size()==arr.length){
-        base.add(new ArrayList<>(list));
-        return;
-    }
+    public static void solution(int arr[],boolean used[],List<Integer> list,
+    List<List<Integer>>  base){
+       if(list.size()==arr.length){
+       base.add(new ArrayList<>(list));
+       return;
+       }
+        for(int i=0;i<arr.length;i++){
+            if(used[i]) continue;
+            used[i]=true;
+            list.add(arr[i]);
+            solution(arr,used,list,base);
+            used[i]=false;
+            list.remove(list.size()-1);
 
-    for(int i=0;i<arr.length;i++){
-        if(used[i]) continue;
-        used[i]=true;
-        list.add(arr[i]);
-        solution(arr,used,base,list);
-        used[i]=false;
-        list.remove(list.size()-1);
-    
-    }
+        }
     }
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result=new ArrayList<>();
-          ArrayList<Integer> list = new ArrayList<>();
-          boolean used[]=new boolean[nums.length];
-         solution(nums,used,result,list);
-         return result;
+        List<List<Integer>>  base = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        boolean used[]=new boolean[nums.length];
+        solution(nums,used,list,base);
+        return base;
         
     }
 }
