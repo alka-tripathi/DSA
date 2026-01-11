0311d85102e075d3081a18936f8f2facce7d959e
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-01-11 14:58:31 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (54.79%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public static boolean solution(TreeNode p,TreeNode q){
+       if(p==null && q==null){
+        return true;
+       }
+       if(p==null || q==null){
+        return false;
+       }
+    
+
+        return ((p.val==q.val) && solution(p.left,q.right) && solution(p.right,q.left));
+
+      
+    }
+    public boolean isSymmetric(TreeNode root) {
+        
+
+        return solution(root.left,root.right);
+
+        
+        
+    }
+}
