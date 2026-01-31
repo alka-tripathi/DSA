a9edd542894544dd9ee5544f768c2c0eff5d3ee8
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-01-31 11:53:19 +0530

    Time: 482 ms (5.03%) | Memory: 46.9 MB (90.68%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
@@ -1,33 +1,27 @@
 class Solution {
-
-    public static int atMostUnique(int arr[],int k){
-        int n=arr.length;
+    public static int atMost(int arr[],int k){
         int count=0;
-
-        for(int i=0;i<n;i++){
-            HashSet<Integer> set = new HashSet<>();
-            for(int j=i;j<n;j++){
+       
+        for(int i=0;i<arr.length;i++){
+             HashSet<Integer> set = new HashSet<>();
+             for(int j=i;j<arr.length;j++){
                 set.add(arr[j]);
                 if(set.size()<=k){
                     count++;
                 }
-            }
+             }
 
         }
         return count;
-
     }
     public int countCompleteSubarrays(int[] nums) {
-
-
         HashSet<Integer> set = new HashSet<>();
         for(int i=0;i<nums.length;i++){
             set.add(nums[i]);
         }
         int k=set.size();
 
-        return atMostUnique(nums,k)-atMostUnique(nums,k-1);
-
+        return atMost(nums,k)-atMost(nums,k-1);
         
     }
 }
