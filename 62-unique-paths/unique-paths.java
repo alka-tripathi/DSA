f83ce5f4bd914ccd46758025458074a02c62e11d
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-02-01 14:43:26 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (77.34%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -12,15 +12,22 @@ class Solution {
         return dp[i][j];
     }
     public int uniquePaths(int m, int n) {
-        int i=0;
-        int j=0;
-        int dp[][]=new int[n][m];
-        for(int k=0;k<n;k++){
-            for(int l=0;l<m;l++){
-                dp[k][l]=-1;
-            }
+      
+        int dp[][]=new int[m][n];
+     for(int i=0;i<n;i++){  //first row
+        dp[0][i]=1;
+     }
+       for(int j=0;j<m;j++){   //first column
+        dp[j][0]=1;
+       }
+      for(int i=1;i<m;i++){
+        for(int j=1;j<n;j++){
+    
+         dp[i][j]=dp[i][j-1]+dp[i-1][j];
+      }
         }
-        return solution(i,j,m,n,dp);
+      
+      return dp[m-1][n-1];
         
     }
 }
