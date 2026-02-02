24f6d6017954f20a0fd9cb7fc3f24ce3227bce18
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-02-02 21:15:40 +0530

    Time: 3 ms (54.20%) | Memory: 45.3 MB (37.69%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,24 +1,23 @@
 class Solution {
-    public static void subset(int arr[],List<Integer> ans ,int i, List<List<Integer>> mainlist){
- if(i>=arr.length){
-    if(!mainlist.contains(ans)){
-        mainlist.add(new ArrayList<>(ans));
+    public static void solution(int i,int arr[],List<Integer> list,List<List<Integer>> result){
+        if(i==arr.length){
+            result.add(new ArrayList<>(list));
+            return;
+        }
+    
+        list.add(arr[i]);
+        solution(i+1,arr,list,result);
+        list.remove(list.size()-1);
+         while(i<arr.length-1 && arr[i]==arr[i+1]){
+            i++;
+        }
+        solution(i+1,arr,list,result);
     }
-    return;
-   
- }
-    ans.add(arr[i]);
-    subset(arr,ans,i+1,mainlist);
-    ans.remove(ans.size()-1);
-    subset(arr,ans,i+1,mainlist);
-   
-}
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-Arrays.sort(nums);
-        List<Integer> ans = new ArrayList<>();
-         List<List<Integer>> mainlist=new ArrayList<>();
-       subset(nums,ans,0,mainlist);
-        return mainlist;
-        
+List<Integer> list=new ArrayList<>();
+        List<List<Integer>> result=new ArrayList<>();
+        Arrays.sort(nums);
+    solution(0,nums,list,result);
+    return result;
     }
 }
