b7217fd9e457614b5f978954a0a4992628eb146d
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-03-04 18:01:40 +0530

    Time: 23 ms (72.00%) | Memory: 50.7 MB (95.80%) - LeetSync

---
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.java
@@ -0,0 +1,67 @@
+class Solution {
+    public static String reverse(String str){
+        char arr[]=str.toCharArray();
+        int i=0;
+        int j=arr.length-1;
+        while(i<j){
+            //swap
+            char temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+        return new String(arr);
+    }
+    public String shortestCommonSupersequence(String str1, String str2) {
+        int n=str1.length();
+        int m=str2.length();
+        int dp[][] =new int[n+1][m+1];
+
+        for(int i=0;i<=m;i++){
+            dp[0][i]=0;
+        }
+        for(int j=0;j<=n;j++){
+            dp[j][0]=0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+              if(str1.charAt(i-1)==str2.charAt(j-1)){
+                dp[i][j]=1+dp[i-1][j-1];
+              }else{
+dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+              }
+            }
+        }
+       //now traverse in dp array
+       int i=n;
+       int j=m;
+       String ans="";
+       while(i>0 && j>0){
+        if(str1.charAt(i-1)==str2.charAt(j-1)){
+            ans+=str1.charAt(i-1);
+            i--;
+            j--;
+        }else if(dp[i-1][j]>dp[i][j-1]){
+            ans+=str1.charAt(i-1);
+            i--;
+        }else{
+            ans+=str2.charAt(j-1);
+            j--;
+        }
+       }
+
+       while(i>0){
+          ans+=str1.charAt(i-1);
+            i--;
+
+       }
+       while(j>0){
+          ans+=str2.charAt(j-1);
+            j--;
+       }
+       return reverse(ans);
+
+        
+    }
+}
