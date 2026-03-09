f81c4754f71ebb2a5160c1b5f80d449ee9a2ee34
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-03-09 08:55:36 +0530

    Time: 5 ms (67.68%) | Memory: 46.5 MB (94.29%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -27,7 +27,24 @@ if(dp[i][j]!= -1)return dp[i][j];
             Arrays.fill(dp[i],-1);
         }
       
-        int ans=solution(n,m,word1,word2,dp);
+      //  int ans=solution(n,m,word1,word2,dp);
+      for(int i=1;i<=n;i++){
+        dp[i][0]=i;
+      }
+      for(int j=1;j<=m;j++){
+        dp[0][j]=j;
+      }
+      dp[0][0]=0;
+      for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(word1.charAt(i-1)==word2.charAt(j-1)){
+                dp[i][j]=dp[i-1][j-1];
+            }else{
+                int min=Math.min(1+dp[i-1][j],1+dp[i][j-1]);
+                dp[i][j]=Math.min(min,1+dp[i-1][j-1]);
+            }
+        }
+      }
         return dp[n][m];
         
     }
