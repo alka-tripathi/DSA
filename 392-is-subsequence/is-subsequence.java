5cfe43d38288367ac38cbdce8e396f69b3d2c423
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-03-13 18:25:51 +0530

    Time: 6 ms (2.38%) | Memory: 43.1 MB (10.92%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.java
@@ -0,0 +1,35 @@
+class Solution {
+    public static boolean solution(int i,int j,String s,String t){
+        if(i<0)return true;
+        if(j<0 && i >=0)return false;
+
+        if(s.charAt(i)==t.charAt(j)){
+            return solution(i-1,j-1,s,t);
+        }
+        return solution(i-1,j,s,t) && solution(i,j-1,s,t);
+    }
+    public boolean isSubsequence(String s, String t) {  
+        int n=s.length();
+        int m=t.length();
+        //return solution(n-1,m-1,s,t);
+        //tabulation
+        boolean dp[][]= new boolean[n+1][m+1];
+        for(int i=0;i<=m;i++){
+            dp[0][i]=true;
+        }
+        for(int i=1;i<=n;i++){
+            dp[i][0]=false;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s.charAt(i-1)==t.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+                }else{
+                    dp[i][j]=dp[i-1][j] && dp[i][j-1];
+                }
+            }
+        }
+        return dp[n][m];
+        
+    }
+}
