a85c2aba5301a82175dc58f8c605c7176b5bd09b
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-03-19 10:02:56 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (77.74%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,33 +1,21 @@
 class Solution {
-    public static int solution(int i,int j,int m,int n,int dp[][]){
-        if(i==n-1 && j==m-1)return 1;
-        if(i>=n || j>=m)return 0;
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
+    public static int solution(int i,int j,int dp[][]){
+        if(i<0 || j<0) return 0;
+        if(i==0 && j==0) return 1;
+          if(dp[i][j] !=-1) return dp[i][j];
+        int up=solution(i-1,j,dp);
+        int left=solution(i,j-1,dp);
 
-        int R=solution(i,j+1,m,n,dp);
-        int D=solution(i+1,j,m,n,dp);
-        dp[i][j]=R+D;
-        return dp[i][j];
+        return dp[i][j]=up+left;
     }
     public int uniquePaths(int m, int n) {
-      
-        int dp[][]=new int[m][n];
-     for(int i=0;i<n;i++){  //first row
-        dp[0][i]=1;
-     }
-       for(int j=0;j<m;j++){   //first column
-        dp[j][0]=1;
-       }
-      for(int i=1;i<m;i++){
-        for(int j=1;j<n;j++){
-    
-         dp[i][j]=dp[i][j-1]+dp[i-1][j];
-      }
-        }
-      
-      return dp[m-1][n-1];
+          int dp[][] = new int[m][n];
+          for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j]=-1;
+            }
+          }
+        return solution(m-1,n-1,dp);
         
     }
 }
