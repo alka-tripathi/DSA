63a306e388cd86e92a05806816f7baca2a52ed03
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-03-28 20:07:32 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (52.90%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,39 +1,26 @@
 class Solution {
-    public static int solution(int arr[]){
-        int n=arr.length;
-        if(n==1) return arr[0];
-        int prev=arr[0];
-        int prev2=0;
-        int curr=0;
-        for(int i=1;i<arr.length;i++){
-            int take=arr[i]+prev2;
-            int nottake=0+prev;
-            curr=Math.max(take,nottake);
-            prev2=prev;
-            prev=curr;
-        }
-        return curr;
+    public static int solution(int i,int arr[],int len,int dp[]){
+      
+        if(i>len) return 0;
+        if(dp[i]!=-1) return dp[i];
+
+        int take=arr[i]+solution(i+2,arr,len,dp);
+        int not_take=solution(i+1,arr,len,dp);
+        return dp[i]=Math.max(take,not_take);
+
+        
     }
     public int rob(int[] nums) {
-        //not tale last element
+
+        //agar phela loge toh last nai le paaoge
+        
         int n=nums.length;
         if(n==1) return nums[0];
-        int temp1[]=new int[n-1];
-         int temp2[]=new int[n-1];
-         int idx1=0;
-         int idx2=0;
-        for(int i=0;i<nums.length;i++){
-            if(i!=0){
-              temp1[idx1++]=nums[i];
-            }
-            if(i!=(n-1)){
-                temp2[idx2++]=nums[i];
-            }
-        }
-        int ans1=solution(temp1);
-        int ans2=solution(temp2);
-        return Math.max(ans1,ans2);
-
+        int dp[] = new int[n];
+        int dp2[] = new int[n];
+        Arrays.fill(dp,-1);
+        Arrays.fill(dp2,-1);
+        return Math.max(solution(0,nums,n-2,dp2),solution(1,nums,n-1,dp));
         
     }
 }
