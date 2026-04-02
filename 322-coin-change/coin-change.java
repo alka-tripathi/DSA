a2f74e2e6f2b025d74c21cba842804cf80210ed4
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-04-02 19:03:15 +0530

    Time: 20 ms (35.80%) | Memory: 46.9 MB (32.15%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,45 +1,48 @@
 class Solution {
     public static int solution(int i,int arr[],int target){
-        if(i==0){
-            if(target%arr[0]==0) return target/arr[0];
-            else return Integer.MAX_VALUE-1;
-        }
+       if(i==0){
+        if(target%arr[0]==0) return target/arr[0];
+        return Integer.MAX_VALUE;
+       }
 
-        int not_take=solution(i-1,arr,target);
+        int nt=solution(i-1,arr,target);
         int take=Integer.MAX_VALUE;
-        if(target>=arr[i]){
-            take=1+solution(i,arr,target-arr[i]);
-         
-                //take=1+res;
-            
+        if(arr[i]<=target){
+            int ans=solution(i,arr,target-arr[i]);
+           if(ans!=Integer.MAX_VALUE){
+            take=1+ans;
+           }
         }
-        return Math.min(take,not_take);
+        return Math.min(take,nt);
     }
-    public int coinChange(int[] coins, int amount) {
-        int n=coins.length;
+    public int coinChange(int[] coins, int target) {
+   int n=coins.length;
+        //return solution(n-1,coins,amount)==Integer.MAX_VALUE?-1:solution(n-1,coins,amount);
 
-          if (amount == 0) return 0;
-        if (coins.length == 0) return -1;
-
-        //int ans= solution(n-1,coins,amount);
-       // return (ans==Integer.MAX_VALUE-1)?-1:ans;
-       int dp[][]= new int[n][amount+1];
-
-       for(int i=0;i<=amount;i++){
-        if(i % coins[0]==0) dp[0][i]=i/coins[0];
-        else dp[0][i]=Integer.MAX_VALUE-1;
-       }
-       for(int i=1;i<n;i++){
-        for(int j=0;j<=amount;j++){
-            int not_take=0+dp[i-1][j];
-            int take=Integer.MAX_VALUE-1;
-            if(j>=coins[i]){
-                take=1+dp[i][j-coins[i]];
-            }
-            dp[i][j]=Math.min(take,not_take);
+        int dp[][]= new int[n+1][target+1];
+        for(int i=0;i<=target;i++){
+            dp[0][i]=Integer.MAX_VALUE;
         }
-       }
-       return (dp[n-1][amount]>=Integer.MAX_VALUE-1)?-1:dp[n-1][amount];
+        for(int i=1;i<=n;i++){
+            dp[i][0]=0;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=target;j++){
+                int nt=dp[i-1][j];
+                int take=Integer.MAX_VALUE;
+
+                if(coins[i-1]<=j){
+                    int ans=dp[i][j-coins[i-1]];
+                    if(ans !=Integer.MAX_VALUE){
+                        take=1+ans;
+                    }
+                  
+                }
+                dp[i][j]=Math.min(nt,take);
+            }
+        }
+        return dp[n][target]==Integer.MAX_VALUE?-1:dp[n][target];
         
     }
 }
