4af4e20912746f5c0ccb477a2bde83909258e5b2
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-04-04 18:25:10 +0530

    Time: 1 ms (70.79%) | Memory: 43.7 MB (24.55%) - LeetSync

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -0,0 +1,27 @@
+class Solution {
+    public static int solution(int i,int arr[],int buy,int dp[][]){
+
+        if(i>=arr.length)return 0;
+
+    if(dp[i][buy]!=-1)return dp[i][buy];
+
+        if(buy==1){
+
+            return  dp[i][buy]= Math.max(-arr[i]+solution(i+1,arr,0,dp),solution(i+1,arr,1,dp));
+        }else{
+            return  dp[i][buy]=Math.max(arr[i]+solution(i+2,arr,1,dp),solution(i+1,arr,0,dp));
+        }
+
+    }
+    public int maxProfit(int[] prices) {
+        int n=prices.length;
+        int dp[][]= new int[n][2];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<2;j++){
+                dp[i][j]=-1;
+            }
+        }
+        return solution(0,prices,1,dp);
+        
+    }
+}
