49a5eb8f353fa568fd0c3a89a954c9fdd5021de7
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-04-06 08:20:12 +0530

    Time: 2 ms (95.59%) | Memory: 45.9 MB (24.75%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,24 +1,25 @@
 class Solution {
-    public static void solution(int arr[],int i,int target,List<Integer> list,  List<List<Integer>>  base){
-        if(i==arr.length || target<0){
-            return;
-        }
+    public static void solution(int i,int arr[],List<Integer> list, List<List<Integer>> result,int target){
         if(target==0){
-           if(!base.contains(list)){
-             base.add(new ArrayList<>(list));
-           }
+            result.add(new ArrayList<>(list));
             return;
         }
+        if(i>=arr.length){
+            return;
+        }
+    if(arr[i]<=target){
         list.add(arr[i]);
-        solution(arr,i+1,target-arr[i],list,base);
-        solution(arr,i,target-arr[i],list,base);
+        solution(i,arr,list,result,target-arr[i]);
          list.remove(list.size()-1);
-          solution(arr,i+1,target,list,base);
+    }
+       
+        solution(i+1,arr,list,result,target);
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>>  base = new ArrayList<>();
-        List<Integer> list= new ArrayList<>();
-        solution(candidates,0,target,list,base);
-        return base;
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        solution(0,candidates,list,result,target);
+        return result;
+        
     }
 }
