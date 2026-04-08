454d8adc11697eb94a8133b7e586e38b44269f74
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-04-08 19:14:01 +0530

    Time: 58 ms (36.10%) | Memory: 63.5 MB (70.06%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -1,52 +1,46 @@
 class Solution {
     public static String reverse(String s){
-        char charS[]=s.toCharArray();
-        int n=s.length();
+        char arr[]=s.toCharArray();
         int l=0;
+        int n=arr.length;
         int r=n-1;
         while(l<=r){
-            //swapdono ko
-            char temp=charS[r];
-            charS[r]=charS[l];
-            charS[l]=temp;
+            //swap
+            char temp=arr[l];
+            arr[l]=arr[r];
+            arr[r]=temp;
             l++;
             r--;
         }
-        return new String(charS);
-
+        return new String(arr);
     }
-    public static int LCS(int i,int j,String s1,String s2){
-        if(i>=s1.length() || j>=s2.length()){
-            return 0;
-        }
- if(s1.charAt(i)==s2.charAt(j)){
-    return 1+LCS(i+1,j+1,s1,s2);
- }
- return Math.max(LCS(i+1,j,s1,s2),LCS(i,j+1,s1,s2));
+
+    public static int solution(int i,int j,String s1,String s2){
+        if(i>=s1.length() || j>=s2.length())return 0;
+
+        if(s1.charAt(i)==s2.charAt(j)){
+            return 1+solution(i+1,j+1,s1,s2);
+        } return Math.max(solution(i+1,j,s1,s2),solution(i,j+1,s1,s2));
+
     }
     public int longestPalindromeSubseq(String s) {
-        String rev= reverse(s);
-        //return LCS(0,0,s,rev);
-        int n=rev.length();
-        int m=s.length();
-        int dp[][]=new int[n+1][m+1];
-        for(int i=0;i<=n;i++){
-            dp[i][0]=0;
-        }
-        for(int i=0;i<=m;i++){
-            dp[0][i]=0;
-        }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s.charAt(i-1)==rev.charAt(j-1)){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }else{
-                    dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
-                }
+        String rev=reverse(s);
 
+       // return solution(0,0,s,rev);
+       int n=s.length();
+
+
+       int dp[][]= new int[n+1][n+1];
+
+       for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(s.charAt(i-1)==rev.charAt(j-1)){
+                dp[i][j]=1+dp[i-1][j-1];
+            }else{
+                dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
             }
         }
-        return dp[n][m];
-        
+       }
+        return dp[n][n];
     }
 }
