9e7756cb73213cd2a1b250ddf04f7e1429a0601c
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-04-09 07:58:22 +0530

    Time: 37 ms (72.20%) | Memory: 46.1 MB (59.16%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -15,18 +15,29 @@ int ans=0;
     public int lengthOfLIS(int[] nums) {
         int len=1;
         int n=nums.length;
-        ArrayList<Integer>arr = new ArrayList<>();
-        arr.add(nums[0]);
-        for(int i=1;i<n;i++){
-            if(nums[i]>arr.get(arr.size()-1)){
-                arr.add(nums[i]);
-                len++;
-            }else{
-                int idx=lowerBound(0,arr.size()-1,nums[i],arr);
-                arr.set(idx,nums[i]);
+        // ArrayList<Integer>arr = new ArrayList<>();
+        // arr.add(nums[0]);
+        // for(int i=1;i<n;i++){
+        //     if(nums[i]>arr.get(arr.size()-1)){
+        //         arr.add(nums[i]);
+        //         len++;
+        //     }else{
+        //         int idx=lowerBound(0,arr.size()-1,nums[i],arr);
+        //         arr.set(idx,nums[i]);
+        //     }
+        // }
+        int dp[]= new int[n];
+        Arrays.fill(dp,1);
+        int maxi=0;
+        for(int i=0;i<n;i++){
+            for(int prev=0;prev<i;prev++){
+                if(nums[i]>nums[prev]){
+                    dp[i]=Math.max(dp[i],1+dp[prev]);
+                }
             }
+            maxi=Math.max(maxi,dp[i]);
         }
-
-        return len;
+return maxi;
+        //return len;
     }
 }
