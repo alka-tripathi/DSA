7f4392d3dacc9ed5135b495a38fc61062c795da3
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-05-01 18:34:45 +0530

    Time: 6 ms (5.87%) | Memory: 42.8 MB (96.22%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+
+        int n=s.length();
+        if(n%2!=0)return false;
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            if(ch=='{' || ch=='[' || ch=='('){
+                stack.push(ch);
+            }else{
+                if(stack.isEmpty())return false;
+              
+              char top=stack.pop();
+              if((ch=='}' && top!='{' ) || (ch==']' && top!='[') || (ch==')' && top!='('))return false;
+            }
+        }
+        return stack.isEmpty();
+        
+    }
+}
