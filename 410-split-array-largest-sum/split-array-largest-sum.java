f5365bd197831dfc973ac3c0326b7910521cbd86
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-05-02 09:55:42 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (7.95%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.java
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -1,35 +1,43 @@
 class Solution {
-    public static boolean isPossible(int arr[],int maxsum,int k){
-        int a=1;
-        int sum=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>maxsum)return false;
-            if(sum+arr[i]<=maxsum){
-                sum+=arr[i];
+    public static boolean canSplit(int sum,int arr[],int k){
+
+        int s=1;
+        int n=arr.length;
+        int currsum=0;
+        for(int i=0;i<n;i++){
+            if(currsum+arr[i]<=sum){
+                currsum+=arr[i];
             }else{
-                sum=arr[i];
-                a++;
+                s++;
+                currsum=arr[i];
+
+                if(s>k)return false; //more than k partition is possible
             }
         }
-        return a<=k;
+        return true;
+
     }
     public int splitArray(int[] nums, int k) {
-        int l=0;
-        int sum=0;
-        for(int i=0;i<nums.length;i++){
-            sum+=nums[i];
+        int n=nums.length;
+        int tsum=0;
+        for(int i=0;i<n;i++){
+            tsum+=nums[i];
         }
-        int r=sum;
-        int ans=-1;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(isPossible(nums,mid,k)){
-                ans=mid;
-                r=mid-1;
-            }else{
-                l=mid+1;
-            }
 
+        int low=0;
+        for(int i=0;i<n;i++){
+            low=Math.max(low,nums[i]);
+        }
+        int high=tsum;
+        int ans=0;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(canSplit(mid,nums,k)){
+                ans=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
         }
         return ans;
         
