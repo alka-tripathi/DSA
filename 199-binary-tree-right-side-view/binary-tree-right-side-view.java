c2ded36c0f80f4892ef1c13a1b2b83907c3749a0
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-06-10 10:35:46 +0530

    Time: 1 ms (73.35%) | Memory: 43.6 MB (49.39%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -14,48 +14,30 @@
  * }
  */
 class Solution {
-    public static List<List<Integer>> level(TreeNode root){
-        List<List<Integer>> result=new ArrayList<>();
-        if(root==null){
-            return result;
-        }
-        List<Integer> list = new ArrayList<>();
+    public static void solution(TreeNode root,List<Integer> ans){
+        if(root==null)return;
         Queue<TreeNode> q = new LinkedList<>();
-        
         q.add(root);
-        q.add(null);
         while(!q.isEmpty()){
-            TreeNode curr=q.remove();
-            if(curr==null){
-                result.add(new ArrayList<>(list));
-                list.clear();
-                
-                if(!q.isEmpty()){
-                     q.add(null);
-                }
-            }else{
-                 list.add(curr.val);
-                if(curr.left!=null){
-                    q.add(curr.left);
-                }
-                if(curr.right!=null){
-                    q.add(curr.right);
-                }
+            int size=q.size();
+            ArrayList<Integer> list = new ArrayList<>();
+            for(int i=0;i<size;i++){
+             TreeNode curr=q.poll();
+             list.add(curr.val);
+             if(curr.left!=null){
+                q.add(curr.left);
+             }
+             if(curr.right!=null){
+                q.add(curr.right);
+             }
             }
+            ans.add(list.get(list.size()-1));
         }
-        return result;
-
     }
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        if(root==null) return ans;
-                List<List<Integer>> result=level(root);
-
-                for(int i=0;i<result.size();i++){
-                   List<Integer> list = result.get(i);
-                   ans.add(list.get(list.size()-1));
-                }
-           return ans; 
+        List<Integer> ans= new ArrayList<>();
+        solution(root,ans);
+        return ans;
         
     }
 }
