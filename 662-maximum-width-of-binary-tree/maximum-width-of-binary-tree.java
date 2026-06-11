336bca8d43eb0d26968cd3d220846f4cf7c1b4eb
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-06-11 10:54:34 +0530

    Time: 2 ms (41.11%) | Memory: 44.9 MB (78.06%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
@@ -14,49 +14,47 @@
  * }
  */
 class Solution {
+    public static int maxans;
     public static class pair{
         TreeNode node;
-        int index;
-        public pair(TreeNode node,int index){
-            this.node=node;
-            this.index=index;
+        int idx;
+        public pair(TreeNode node,int idx){
+            this.node = node;
+            this.idx=idx;
+        }
+    }
+    public static void solution(TreeNode root){
+        if(root==null)return ;
+        Deque<pair> q = new ArrayDeque<>();
+        q.addLast(new pair(root,0));
+
+
+       
+            
+        while(!q.isEmpty()){
+            int size=q.size();
+          
+              int f=q.peekFirst().idx;
+            int l=q.peekLast().idx;
+            maxans=Math.max(l-f+1,maxans);
+           
+
+          for(int i=0;i<size;i++){
+            pair p = q.removeFirst();
+                TreeNode node = p.node;
+                int idx = p.idx;
+              if(node.left!=null){
+                q.addLast(new pair(node.left,2*idx+1));
+            }
+             if(node.right!=null){
+                q.addLast(new pair(node.right,2*idx+2));
+            }
+          }
         }
     }
     public int widthOfBinaryTree(TreeNode root) {
-
-        Deque<pair> dq= new ArrayDeque<>();
-        int maxans=0;
-
-        dq.addLast(new pair(root,0));
-
-        while(!dq.isEmpty()){
-           int size = dq.size();
-
-            int fi = dq.peekFirst().index;
-            int li = dq.peekLast().index;
-             maxans = Math.max(maxans, li - fi + 1);
-           
-
-            for(int i=0;i<size;i++){
-
-               pair curr=dq.removeFirst();
-             int idx=curr.index;
-
-
-                if(curr.node.left != null){
-                    pair newp=new pair(curr.node.left,2*idx+1);
-                  dq.addLast(newp);
-                }
-                if(curr.node.right!=null){
-                 pair newp1=new pair(curr.node.right,2*idx+2);
-                  dq.addLast(newp1);
-                }
-
-            }
-
-        }
+maxans=0;
+         solution(root);
         return maxans;
-
-        
     }
 }
