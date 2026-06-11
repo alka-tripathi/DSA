8d8d6652ea8f865f091d4a41f7d581b59284e9fc
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-06-11 19:18:07 +0530

    Time: 4 ms (39.25%) | Memory: 43.3 MB (40.24%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,18 +1,21 @@
 class Solution {
     public boolean isValid(String s) {
-
-        int n=s.length();
-        if(n%2!=0)return false;
         Stack<Character> stack = new Stack<>();
-        for(int i=0;i<n;i++){
+        for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            if(ch=='{' || ch=='[' || ch=='('){
+            if(ch=='(' || ch=='{' || ch=='['){
                 stack.push(ch);
-            }else{
-                if(stack.isEmpty())return false;
-              
-              char top=stack.pop();
-              if((ch=='}' && top!='{' ) || (ch==']' && top!='[') || (ch==')' && top!='('))return false;
+            }else {
+               if(!stack.isEmpty()){
+                 char c=stack.peek();
+                if((c=='(' && ch==')') || (c=='[' && ch==']') || (c=='{' && ch=='}')){
+                    stack.pop();
+                }else{
+                    return false;
+               }
+               }else{
+                return false;
+               }
             }
         }
         return stack.isEmpty();
