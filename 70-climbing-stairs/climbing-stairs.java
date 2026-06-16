e8702d4d3885ba52ffbee5deab151d21144bd576
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-06-16 09:43:01 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (53.28%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,22 +1,16 @@
 class Solution {
-    public static int solution(int i,int n,int dp[]){
-        if(i>=n-1) return 1;
+    public static int sol(int n,int dp[]){
+        if(n==1)return 1;
+        if(n==2)return 2;
+        if(dp[n]!=-1)return dp[n];
 
-        if(dp[i]!=-1) return dp[i];
-
-        int first=solution(i+1,n,dp);
-        int second = solution(i+2,n,dp);
-
-
-        return dp[i]=first + second;
+        return dp[n]=sol(n-1,dp)+sol(n-2,dp);
     }
     public int climbStairs(int n) {
-        int dp[]= new int[n];
-        for(int i=0;i<n;i++){
-            dp[i]=-1;
-        }
+        int dp[]=new int[46];
+       Arrays.fill(dp,-1);
 
-        return solution(0,n,dp);
+        return sol(n,dp);
         
     }
 }
