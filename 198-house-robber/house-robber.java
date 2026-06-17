ef29daf081af16847ecb0bcec869129c2f92f7d1
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-06-17 09:47:19 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (5.30%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,21 +1,31 @@
 class Solution {
-    public static int solution(int i,int arr[],int dp[]){
-          if(i==arr.length-1) return arr[arr.length-1];
-       if(i>=arr.length) return 0;
-       if(dp[i] != -1) return dp[i];
-     
-       int take=arr[i]+solution(i+2,arr,dp);
-       int not_take=solution(i+1,arr,dp);
-       
-        return dp[i]= Math.max(take,not_take);
+    public static int sol(int arr[],int i,int dp[]){
+           if(i==0)return arr[0];
+        if(i<0)return 0;
+        if(dp[i] != -1)return dp[i];
 
+        int nt=sol(arr,i-1,dp);
+        int t=arr[i]+sol(arr,i-2,dp);
+        return dp[i]=Math.max(nt,t);
     }
     public int rob(int[] nums) {
         int n=nums.length;
-        int dp[]= new int[n];
-        Arrays.fill(dp,-1);
+        int dp[]= new int[n+1];
+        // Arrays.fill(dp,-1);
+        // return sol(nums,n-1,dp);
 
-        return solution(0,nums,dp);
-        
+        dp[0]=0;
+        dp[1]=nums[0];
+
+        for(int i=2;i<=n;i++){
+            int nt=dp[i-1];
+          
+           int  t=nums[i-1]+dp[i-2];
+         
+            dp[i]=Math.max(nt,t);
+        }
+        return dp[n];
+
+    
     }
 }
