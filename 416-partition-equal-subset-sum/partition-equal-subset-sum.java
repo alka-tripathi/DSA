e0b0db197e32ea987bd018fb77f13e250deaae52
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-06-19 10:16:26 +0530

    Time: 50 ms (47.83%) | Memory: 47.5 MB (54.36%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,42 +1,32 @@
 class Solution {
-    public static boolean solution(int i,int arr[],int target){
-        if(target==0) return true;
-        if(i>=arr.length) return false;
-
-        boolean nt=solution(i+1,arr,target);
-        boolean take=false;
-        if(target>=arr[i]){
-            take=solution(i+1,arr,target-arr[i]);
-        }
-        return nt || take;
-    }
     public boolean canPartition(int[] nums) {
 
-        int sum=0;
-        int n=nums.length;
+     int sum=0;
+     int n=nums.length;
+     for(int i=0;i<nums.length;i++){
+        sum+=nums[i];
+     }
+     if(sum%2!=0)return false;
+     int target=sum/2;
+
+        boolean dp[][]= new boolean[n][target+1];
+
         for(int i=0;i<n;i++){
-            sum+=nums[i];
-        }
-        if(sum%2 !=0) return false;
-        int target=sum/2;
-        //return solution(0,nums,sum/2);
-        boolean dp[][]= new boolean[n+1][target+1];
-        for(int i=0;i<=n;i++){
             dp[i][0]=true;
         }
-        for(int i=1;i<=n;i++){
+
+        for(int i=1;i<n;i++){
             for(int j=1;j<=target;j++){
                 boolean nt=dp[i-1][j];
-                boolean take=false;
-                if(nums[i-1]<=j){
-                    take=dp[i-1][j-nums[i-1]];
+                boolean t=false;
+                if(nums[i]<=j){
+                    t=dp[i-1][j-nums[i]];
                 }
-
-                dp[i][j]=take|| nt;
-
+                dp[i][j]=t || nt;
             }
         }
-        return dp[n][target];
+        return dp[n-1][target];
+
         
     }
 }
