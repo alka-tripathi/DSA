ae0249856f5acfc9ebe97db2ec7d7ac92c8ca0da
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-06-21 10:23:21 +0530

    Time: 191 ms (33.18%) | Memory: 43 MB (86.44%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,29 +1,30 @@
 class Solution {
-    public static int solution(int i,int arr[],int target){
-       
-       if(i == 0){
-            if(target == 0 && arr[0] == 0) return 2;
-            if(target == 0 || target == arr[0]) return 1;
-            return 0;
-        }
-
-           int not_take=solution(i-1,arr,target);
-
-         int take=0;
-       if(target>=arr[i]){
-         take=solution(i-1,arr,target-arr[i]);
-       }
+    public static int sol(int i,int arr[],int sum){
+    
+if(i==0){
+    if(sum==0 && arr[i]==0)return 2;
+    if(sum==0 || arr[i]==sum)return 1;
+    return 0;
+}
      
-        return take+not_take;
+  int nt=sol(i-1,arr,sum);
+  int t=0;
+  if(sum>=arr[i]){
+    t=sol(i-1,arr,sum-arr[i]);
+  }
+  return nt+t;
+
+        
     }
     public int findTargetSumWays(int[] nums, int target) {
-        int n=nums.length;
-        int sum=0;
-        for(int a :nums) sum+=a;
-        if((sum+target)%2!=0 ) return 0;
-        int newT=(sum+target)/2;
-        
-        return solution(n-1,nums,newT);
+int n=nums.length;
+       int sum=0;
+       for(int i=0;i<n;i++){
+        sum+=nums[i];
+       }
+       if((target+sum)%2!=0)return 0;
+
+       return sol(n-1,nums,(target+sum)/2);
         
     }
 }
