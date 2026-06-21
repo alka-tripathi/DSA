158b36e52d4acf41c3b7c66086e0b58c615beb93
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-06-21 10:36:33 +0530

    Time: 15 ms (38.55%) | Memory: 57.3 MB (22.04%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,36 +1,29 @@
 class Solution {
-    public static int solution(int i, int arr[], int target, int dp[][]) {
-        //base case
-        if (i == 0) {
-            if (target % arr[0] == 0)
-                return 1;
-            else {
-                return 0;
-            }
+    public static int sol(int i,int arr[],int amount,int dp[][]){
+    if(amount==0)return 1;
+      if(i==0){
+        if(amount%arr[0]==0)return 1;
+        return 0;
+      }
 
+if(dp[i][amount]!=-1)return dp[i][amount];
+        int nt=sol(i-1,arr,amount,dp);
+        int t=0;
+        if(arr[i]<=amount){
+            t=sol(i,arr,amount-arr[i],dp);
         }
-        if (dp[i][target] != -1) {
-            return dp[i][target];
-        }
-
-        int take = 0;
-        int nottake =  solution(i - 1, arr, target, dp);
-        if (arr[i] <= target) {
-            take = solution(i, arr, target - arr[i], dp);
-        }
-        dp[i][target] = take + nottake;
-        return dp[i][target];
-
+        return dp[i][amount]=nt+t;
     }
-
     public int change(int amount, int[] coins) {
-        int n = coins.length;
-        int dp[][] = new int[n][amount + 1];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-       
-     
-        return  solution(n - 1, coins, amount, dp);
+
+    int n=coins.length;
+    int dp[][]= new int[n][amount+1];
+    for(int i=0;i<n;i++){
+        Arrays.fill(dp[i],-1);
+    }
+  return sol(n-1,coins,amount,dp);
+
+        
+        
     }
 }
