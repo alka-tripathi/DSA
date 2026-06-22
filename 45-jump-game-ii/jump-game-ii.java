b9dc2da4180b4f0e9beaa9a93b6a12bab0277fed
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-06-22 20:58:43 +0530

    Time: 124 ms (6.60%) | Memory: 47.5 MB (21.15%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,21 +1,22 @@
 class Solution {
+    public static int sol(int i,int arr[],int dp[]){
+        if(i>=arr.length-1)return 0;
+int ans=Integer.MAX_VALUE;
+if(dp[i]!=-1)return dp[i];
+
+for(int j=1;j<=arr[i];j++){
+    int temp=sol(j+i,arr,dp);
+    if(temp!=Integer.MAX_VALUE){
+        ans=Math.min(ans,1+temp);
+    }
+
+}
+return dp[i]=ans;
+    }
     public int jump(int[] nums) {
-
         int n=nums.length;
-        if(n==1)return 0;
-        int maxreach=0;
-        int endrange=0;
-        int ans=0;
-        for(int i=0;i<n;i++){
-            maxreach=Math.max(maxreach,nums[i]+i);
-            if(i==endrange){
-                ans++;
-                endrange=maxreach;
-                if(endrange>=(n-1))break;
-            }
-
-        }
-        return ans;
-        
+        int dp[]= new int[n];
+        Arrays.fill(dp,-1);
+        return sol(0,nums,dp);
     }
 }
