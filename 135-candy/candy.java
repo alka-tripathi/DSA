e36cf4614391ebc4cb28af6037e8ea2defdc5b60
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-06-24 22:57:47 +0530

    Time: 2 ms (99.77%) | Memory: 46.9 MB (99.29%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n=ratings.length;
+       int ans[]= new int[n];
+       Arrays.fill(ans,1);
+
+       for(int i=1;i<n;i++){
+        if(ratings[i]>ratings[i-1]){
+            ans[i]=1+ans[i-1];
+        }
+       }
+
+       for(int i=n-2;i>=0;i--){
+        if(ratings[i]>ratings[i+1]){
+            ans[i]=Math.max(ans[i],ans[i+1]+1);
+        }
+       }
+       int c=0;
+       for(int i:ans){
+        c+=i;
+       }
+       return c;
+        
+    }
+}
