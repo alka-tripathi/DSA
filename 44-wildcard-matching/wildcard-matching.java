7635a927e276573b3bd1f0fea2927d6a6f371f78
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-06-26 09:55:43 +0530

    Time: 17 ms (71.81%) | Memory: 63.8 MB (8.66%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,32 +1,33 @@
 class Solution {
-    public static Boolean solution(int i,int j,String s,String p,Boolean dp[][]){
-         if(i<0 && j<0)return true;
-         if(i>=0 && j<0)return false;
-         if(i<0 && j>=0){
+    public static boolean sol(int i,int j,String s,String p,Boolean dp[][]){
+          // Both string and pattern finished
+        if(i<0 && j<0)return true;
+
+         // Pattern finished but string still left
+        if(j<0 && i>=0)return false;
+
+          // String finished but pattern left
+        if(j>=0 && i<0){
             for(int k=0;k<=j;k++){
                 if(p.charAt(k)!='*')return false;
             }
             return true;
-         }
-         if(dp[i][j] !=null){
-            return dp[i][j];
-         }
+        }
+        if(dp[i][j]!=null)return dp[i][j];
 
-        if(s.charAt(i)==p.charAt(j) || p.charAt(j) == '?'){
-            return dp[i][j]=solution(i-1,j-1,s,p,dp);
+        if(s.charAt(i)==p.charAt(j) || p.charAt(j)=='?'){
+            return dp[i][j]=sol(i-1,j-1,s,p,dp);
         }
         if(p.charAt(j)=='*'){
-            return dp[i][j]=solution(i-1,j,s,p,dp) || solution(i,j-1,s,p,dp); 
+            return dp[i][j]=sol(i-1,j,s,p,dp) || sol(i,j-1,s,p,dp);
         }
         return false;
     }
     public boolean isMatch(String s, String p) {
-
-        int n=s.length();
-        int m=p.length();
-         Boolean dp[][] = new Boolean[n+1][m+1];
-
-        return solution(n-1,m-1,s,p,dp);
+int n=s.length();
+int m=p.length();
+Boolean dp[][]= new Boolean[n][m];
+        return sol(n-1,m-1,s,p,dp);
         
     }
 }
