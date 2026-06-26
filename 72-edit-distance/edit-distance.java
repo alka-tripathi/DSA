e1822d359a92bb1376f2a5447e887e7faccd4729
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-06-26 11:14:54 +0530

    Time: 3 ms (98.32%) | Memory: 47.1 MB (52.52%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,25 +1,34 @@
 class Solution {
-    public static int solution(int i,int j,String str1,String str2,int dp[][]){
-        if(j<0)return i+1;
-        if(i<0)return j+1;
-        if(dp[i][j]!=-1)return dp[i][j];
 
-        if(str1.charAt(i)==str2.charAt(j)){
-            return dp[i][j]=solution(i-1,j-1,str1,str2,dp);
-        }
-        int minO=Math.min(1+solution(i-1,j,str1,str2,dp),1+solution(i,j-1,str1,str2,dp));
-        return dp[i][j]=Math.min(minO,1+solution(i-1,j-1,str1,str2,dp));
+  public static int sol(int i,int j,String s1,String s2,int dp[][]){
+    if(i<0)return j+1;
+    if(j<0)return i+1;
+    if(dp[i][j]!=-1)return dp[i][j];
+
+    if(s1.charAt(i)==s2.charAt(j)){
+        return dp[i][j]=sol(i-1,j-1,s1,s2,dp);
     }
+    int del=1+sol(i-1,j,s1,s2,dp);
+    int ins=1+sol(i,j-1,s1,s2,dp);
+    int rep=1+sol(i-1,j-1,s1,s2,dp);
+
+    return dp[i][j]=Math.min(Math.min(del,ins),rep);
+    
+  }
+
     public int minDistance(String word1, String word2) {
-     int n=word1.length();
-     int m=word2.length();
-     int dp[][]= new int[n+1][m+1];
-     for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            dp[i][j]=-1;
-        }
-     }
-        return solution(n-1,m-1,word1,word2,dp);
+
+        int n=word1.length();
+        int m=word2.length();
+
+  int dp[][]= new int[n][m];
+  for(int i=0;i<n;i++){
+  Arrays.fill(dp[i],-1);
+  }
+        return sol(n-1,m-1,word1,word2,dp);
+
         
+        
+
     }
 }
