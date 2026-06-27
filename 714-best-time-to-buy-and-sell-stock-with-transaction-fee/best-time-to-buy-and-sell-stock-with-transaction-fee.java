3fb555ca0519d797d3f4167035355a6587317a50
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-06-27 23:46:06 +0530

    Time: 38 ms (11.32%) | Memory: 78.9 MB (12.74%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,23 +1,25 @@
 class Solution {
-    public static int solution(int i,int arr[],int fee,int buy,int dp[][]){
-        if(i>=arr.length)return 0;
-        if(dp[i][buy]!=-1)return dp[i][buy];
+    public static int sol(int i,int arr[],int buy,int fee,int dp[][]){
+    
+    if(i>=arr.length)return 0;
 
-        if(buy==1){
-            return dp[i][buy]= Math.max(-arr[i]+solution(i+1,arr,fee,0,dp),solution(i+1,arr,fee,1,dp));
-        }else{
-            return dp[i][buy]= Math.max(arr[i]+solution(i+1,arr,fee,1,dp)-fee,solution(i+1,arr,fee,0,dp));
-        }
+    if(dp[i][buy]!=-1)return dp[i][buy];
+
+   
+   if(buy==1){
+    return dp[i][buy]=Math.max(-arr[i]+sol(i+1,arr,0,fee,dp),sol(i+1,arr,1,fee,dp));
+   }else{
+    return dp[i][buy]=Math.max(arr[i]+sol(i+1,arr,1,fee,dp)-fee,sol(i+1,arr,0,fee,dp));
+   }
+
     }
     public int maxProfit(int[] prices, int fee) {
         int n=prices.length;
-int dp[][]= new int[n][2];
-for(int i=0;i<n;i++){
-    for(int j=0;j<2;j++){
-        dp[i][j]=-1;
-    }
-}
-        return solution(0,prices,fee,1,dp);
+        int dp[][]= new int[n][2];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return sol(0,prices,1,fee,dp);
         
     }
 }
