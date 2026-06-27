e3fcd78156c88301c6173280b4112618a6c134c7
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-06-27 23:27:03 +0530

    Time: 1 ms (70.19%) | Memory: 43.6 MB (31.86%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,27 +1,22 @@
 class Solution {
-    public static int solution(int i,int arr[],int buy,int dp[][]){
-
+    public static int sol(int i,int arr[],int buy,int dp[][]){
         if(i>=arr.length)return 0;
 
-    if(dp[i][buy]!=-1)return dp[i][buy];
+        if(dp[i][buy]!=-1)return dp[i][buy];
 
         if(buy==1){
-
-            return  dp[i][buy]= Math.max(-arr[i]+solution(i+1,arr,0,dp),solution(i+1,arr,1,dp));
+            return dp[i][buy]=Math.max(-arr[i]+sol(i+1,arr,0,dp),sol(i+1,arr,1,dp));
         }else{
-            return  dp[i][buy]=Math.max(arr[i]+solution(i+2,arr,1,dp),solution(i+1,arr,0,dp));
+            return dp[i][buy]= Math.max(arr[i]+sol(i+2,arr,1,dp),sol(i+1,arr,0,dp));
         }
-
     }
     public int maxProfit(int[] prices) {
         int n=prices.length;
         int dp[][]= new int[n][2];
         for(int i=0;i<n;i++){
-            for(int j=0;j<2;j++){
-                dp[i][j]=-1;
-            }
+            Arrays.fill(dp[i],-1);
         }
-        return solution(0,prices,1,dp);
+        return sol(0,prices,1,dp);
         
     }
 }
