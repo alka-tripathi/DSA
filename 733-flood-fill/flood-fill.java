20f99095978537b903a7bc26c7e1ffc12aebb2c1
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-06-28 18:42:38 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (89.91%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,34 +1,28 @@
 class Solution {
-    public static int  direction[][]={{1,0},{-1,0},{0,1},{0,-1}};
-    public static void dfs(int image[][],int i,int j,int color,int curr){
-        if(i>=image.length || j>=image[0].length || i<0 || j<0){
+    //dfs used here
+    public static int direction[][]={{0,-1},{-1,0},{1,0},{0,1}};
+    public static void dfs(int image[][],int i,int j,int ogColor,int newColor){
+
+        if(i<0 || j<0 || i>=image.length || j>=image[0].length || image[i][j]!=ogColor || image[i][j]==newColor){
             return;
         }
-        if(image[i][j]!=curr)return;
-       
-        image[i][j]=color;
+
+        image[i][j]=newColor;
+        //now go to 4 direction
         for(int k=0;k<4;k++){
             int new_i=direction[k][0]+i;
             int new_j=direction[k][1]+j;
-            if(new_i>=0 && new_i<image.length && new_j>=0 && new_j<image[0].length && image[new_i][new_j]==curr && image[new_i][new_j]!=color){
-                dfs(image,new_i,new_j,color,curr);
+
+            if(new_i>=0 && new_j>=0 && new_j<=image.length && new_i<=image.length){
+                dfs(image,new_i,new_j,ogColor,newColor);
             }
         }
-      
-
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-    int n=image.length;
-    int m=image[0].length;
-    int curr=image[sr][sc];
-       
-                     dfs(image,sr,sc,color,curr);
-                     return image;
-                
 
-            
-        }
-    
+        int n=image.length;
+         dfs(image,sr,sc,image[sr][sc],color);
+         return image;
         
     }
-    
+}
