b5937d22235e48d196dd0584af84024204c9f40d
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-06-29 10:33:28 +0530

    Time: 18 ms (95.89%) | Memory: 45.4 MB (57.79%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
@@ -1,35 +1,38 @@
 class Solution {
     public int findNumberOfLIS(int[] nums) {
-        int n=nums.length;
-        int dp[]=new int[n];
-            Arrays.fill(dp,1);
 
+        int n=nums.length;
         int count[]= new int[n];
+        int dp[]= new int[n];
+        Arrays.fill(dp,1);
         Arrays.fill(count,1);
+        int lastI=1;
+       int maxlen=1;
         for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
-             if(nums[j]<nums[i]){
-                   if(dp[j]+1==dp[i]){
-                    count[i]+=count[j];
+                if(nums[j] < nums[i]) {
+                if(dp[i]==dp[j]+1){  //same mill gya
+                  count[i]+=count[j]; //peeche se bhi add hoga
                 }else if(dp[j]+1>dp[i]){
                     dp[i]=dp[j]+1;
                     count[i]=count[j];
                 }
-             }
+                }
+            }
+           if(maxlen<dp[i]){
+            maxlen=dp[i];
+            lastI=i;
+           }
+        }
+//maxlen ka hi count nikalna hai
+
+        
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(dp[i]==maxlen){
+                ans+=count[i];
             }
         }
-     int maxlen=0;
-     for(int i=0;i<n;i++){
-        maxlen=Math.max(maxlen,dp[i]);
-     }
-     int ans=0;
-
-     for(int i=0;i<n;i++){
-        if(dp[i]==maxlen){
-            ans+=count[i];
-        }
-     }
-     return ans;
-        
+        return ans;
     }
 }
