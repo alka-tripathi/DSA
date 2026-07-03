7bd302d8a7864d08cdf3731d9aadfcb60bb843a2
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-07-03 20:49:09 +0530

    Time: 118 ms (5.78%) | Memory: 86 MB (7.70%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,55 +1,63 @@
 class Solution {
-    //nextSmallest elements
-    public static ArrayList<Integer> nse(int arr[]){
+    public static int[] nse(int arr[]){
         Stack<Integer> s = new Stack<>();
-        ArrayList<Integer> ans = new ArrayList<>();
         int n=arr.length;
+        
+        int ans[]= new int[n];
         for(int i=n-1;i>=0;i--){
-            while(!s.isEmpty() && arr[s.peek()]>=arr[i]){
-                s.pop();
-            }
             if(s.isEmpty()){
-                ans.add(n);
+                ans[i]=n;
             }else{
-                ans.add(s.peek());
+                while(!s.isEmpty() && arr[s.peek()]>=arr[i]){
+                    s.pop();
+                }
+                if(s.isEmpty()){
+                    ans[i]=n;
+                }else{
+                    ans[i]=s.peek();
+                }
             }
             s.push(i);
-
         }
-          Collections.reverse(ans);
         return ans;
+
+
     }
 
-    //previous smallest numvber
-    public static ArrayList<Integer> pse(int arr[]){
+      public static int[] pse(int arr[]){
         Stack<Integer> s = new Stack<>();
-        ArrayList<Integer> ans = new ArrayList<>();
         int n=arr.length;
+        
+        int ans[]= new int[n];
         for(int i=0;i<n;i++){
-            while(!s.isEmpty() && arr[s.peek()]>=arr[i]){
-                s.pop();
-            }
             if(s.isEmpty()){
-        ans.add(-1);
+                ans[i]=-1;
             }else{
-                ans.add(s.peek());
-
+                while(!s.isEmpty() && arr[s.peek()]>arr[i]){
+                    s.pop();
+                }
+                if(s.isEmpty()){
+                    ans[i]=-1;
+                }else{
+                    ans[i]=s.peek();
+                }
             }
             s.push(i);
         }
-      //  Collections.reverse(ans);
         return ans;
+
+
     }
     public int largestRectangleArea(int[] heights) {
+        int n=heights.length;
+        int nse[]=nse(heights);
+        int pse[]=pse(heights);
 
-        ArrayList<Integer> nse1= nse(heights);
-         ArrayList<Integer>  pse1 = pse(heights);
-         int maxArea=Integer.MIN_VALUE;
-         for(int i=0;i<nse1.size();i++){
-            int w=(nse1.get(i)-pse1.get(i))-1;
-            maxArea=Math.max(maxArea,heights[i]*w);
-         }
-return maxArea;
-        
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int w=nse[i]-pse[i]-1;
+            ans=Math.max(ans,w*heights[i]);
+        }
+        return ans;
     }
 }
