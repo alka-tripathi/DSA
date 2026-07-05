e19ade16e845d1370152c490f552398099bad58e
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-07-05 23:03:54 +0530

    Time: 31 ms (60.78%) | Memory: 56.5 MB (13.28%) - LeetSync

--- 937-online-stock-span/online-stock-span.java
+++ 937-online-stock-span/online-stock-span.java
@@ -1,29 +1,27 @@
 class StockSpanner {
- static class Pair{
-    int val;
-    int span;
-    public Pair(int val,int span){
-        this.val=val;
-        this.span=span;
+    public static class pair{
+        int val;
+        int span;
+        public pair(int val,int span){
+            this.val=val;
+            this.span=span;
+        }
     }
-}
+    public static Stack<pair> s;
 
-    
-Stack<Pair> s ;
     public StockSpanner() {
-        s=new Stack<>();
+      s = new Stack<>();
         
     }
     
     public int next(int price) {
         int span=1;
         while(!s.isEmpty() && s.peek().val<=price){
-           span+=s.peek().span;
+            span+=s.peek().span;
             s.pop();
         }
-        s.push(new Pair(price,span));
+        s.push(new pair(price,span));
         return span;
-        
     }
 }
 
