a2360a28c7a75c26ce89c0b39a34f00e80586766
Alka Tripathi <160255609+alka-tripathi@users.noreply.github.com>
2026-07-08 16:09:02 +0530

    Time: 16 ms (36.72%) | Memory: 46.4 MB (39.14%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,38 +1,37 @@
 class Solution {
-    public static int Maxfrequency(int arr[]){
-        int maxF=Integer.MIN_VALUE;
-        int n=arr.length;
-        for(int i=0;i<n;i++){
-            maxF=Math.max(maxF,arr[i]);
-
+    public static int maxFreq(int arr[]){
+        int max_value=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            max_value=Math.max(arr[i],max_value);
         }
-        return maxF;
+        return max_value;
     }
+
     public int characterReplacement(String s, int k) {
+
         int n=s.length();
-        int freq[]= new int[26];
-        int maxlen=0;
+        int hash[]= new int[26];
         int l=0;
         int r=0;
+        int maxF=0;
+        int maxlen=0;
 
         while(r<n){
             char ch=s.charAt(r);
-            freq[ch-'A']++;
-            int maxFreq=Maxfrequency(freq);
+            hash[ch-'A']++;
+            maxF=maxFreq(hash);
+           
+            while((r-l+1)-maxF>k){
+                hash[s.charAt(l)-'A']--;
+              
+                //now update the maxFrequency
 
+               maxF=maxFreq(hash);
+               l++;
 
-            while((r-l+1)-maxFreq >k){
-                //shrink;
-                char c=s.charAt(l);
-                freq[c-'A']--;
-                 maxFreq=Maxfrequency(freq);
-                 l++;
-
+                
             }
-
-            if((maxlen-maxFreq)<=k){
-                maxlen=Math.max(maxlen,r-l+1);
-            }
+            maxlen=Math.max(maxlen,r-l+1);
             r++;
 
         }
